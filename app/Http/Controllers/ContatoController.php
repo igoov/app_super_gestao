3409367779b3ad5aa56af869df4d84a035a7319d
igor <igorbernardorocha@gmail.com>
2026-04-24 03:44:34 -0300

    implementacao de controladores

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ContatoController extends Controller
+{
+    public function contato()
+    {
+        return 'Contato';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreNosController;
 
-Route::get('/', function () {
-    return 'Ola, seja bem vindo!!';
-});
+Route::get('/', [PrincipalController::class, 'principal']);
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre nós';
-});
+Route::get('/contato', [ContatoController::class, 'contato']);
 
-Route::get('/contato', function () {
-    return 'nicole gostosa!';
-}); 
+Route::get('/sobre-nos', [SobreNosController::class, 'index']);
 
 /* Verbo http 
 
