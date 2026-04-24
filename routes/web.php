d9900e0375594d38f30604dafa31f4608c2604f4
igor <igorbernardorocha@gmail.com>
2026-04-24 03:13:16 -0300

    segundo commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ola, seja bem vindo!!';
 });
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre nós';
+});
+
+Route::get('/contato', function () {
+    return 'nicole gostosa!';
+}); 
+
+/* Verbo http 
+
+get
+post
+put
+patch 
+delete 
+options
+
+*/
