f6bfc302b043f5a55df44dbcd4f912c36ef39f51
igor <igorbernardorocha@gmail.com>
2026-04-25 19:37:05 -0300

    implementando rotas de: login, produtos, fornecedors, clientes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\SobreNosController;
 
 Route::get('/', [PrincipalController::class, 'principal']);
-
 Route::get('/contato', [ContatoController::class, 'contato']);
-
 Route::get('/sobre-nos', [SobreNosController::class, 'index']);
 //nome, categoria, assunto, mensagem
+
+Route::get('/login', function () { return 'Login'; });
+Route::get('/clientes', function () { return 'Clientes'; });
+Route::get('/fornecedores', function () { return 'Fornecedores'; });
+Route::get('/produtos', function () { return 'Produtos'; });
 
 /* Verbo http 
 get
