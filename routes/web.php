2464e4a8594fe61e721c389f8c8ef92a51518e84
igor <igorbernardorocha@gmail.com>
2026-04-24 21:17:17 -0300

    paremeros opcionais

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,23 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\SobreNosController;
 
-Route::get('/', [PrincipalController::class, 'Principal']);
+Route::get('/', [PrincipalController::class, 'principal']);
 
-Route::get('/contato', [ContatoController::class, 'Contato']);
+Route::get('/contato', [ContatoController::class, 'contato']);
 
-Route::get('/sobre-nos', [SobreNosController::class, 'Index']);
+Route::get('/sobre-nos', [SobreNosController::class, 'index']);
 //nome, categoria, assunto, mensagem
 
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}',
-    function(string $nome, string $categoria, string $assunto, string $mensagem){
+Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+    function(
+        string $nome = 'desconhecido', 
+        string $categoria = 'Informacao', 
+        string $assunto = 'Contato', 
+        string $mensagem = 'mensagem nao informada'
+        ){
         echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
-});
+    }
+);
 
 /* Verbo http 
 
