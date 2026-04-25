57026747b8725a61b6702292db26560af1730bd7
igor <igorbernardorocha@gmail.com>
2026-04-25 19:51:26 -0300

    nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,17 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\SobreNosController;
 
-Route::get('/', [PrincipalController::class, 'principal']);
-Route::get('/contato', [ContatoController::class, 'contato']);
-Route::get('/sobre-nos', [SobreNosController::class, 'index']);
-Route::get('/login', function () { return 'Login'; });
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/sobre-nos', [SobreNosController::class, 'index'])->name('site.sobrenos');
+Route::get('/login', function () { return 'Login'; })->name('site.login');
 //nome, categoria, assunto, mensagem
 
 //app
 Route::prefix('/app')->group(function (){
-    Route::get('/clientes', function () { return 'Clientes'; });
-    Route::get('/fornecedores', function () { return 'Fornecedores'; });
-    Route::get('/produtos', function () { return 'Produtos'; });
+    Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function () { return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
 });
 
 /* Verbo http 
