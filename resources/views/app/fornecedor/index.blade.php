4ddf1ed32ff530f1cec4af791b9430f4192957e8
igor <igorbernardorocha@gmail.com>
2026-04-26 21:53:39 -0300

    cometarios no laravel e php

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,15 @@
+<h3>Fornecedor</h3>
+
+{{ 'Texto de teste'}}
+
+{{--Fica o comentario que sera descartadoo peo interpretador do blade--}}
+
+@php
+    //para comentarios de uma linha
+
+    /*
+        Para comentarios de multiplas linhas
+    */
+
+    echo 'Texto de teste';
+@endphp
