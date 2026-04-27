f70a55e4fc46e74d9c817c982ff38221328cd8c8
igor <igorbernardorocha@gmail.com>
2026-04-26 22:19:52 -0300

    unless, endunless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -5,13 +5,22 @@
 {{--Fica o comentario que sera descartadoo peo interpretador do blade--}}
 
     @php
-    
+        /*
+        if  (!<condicao>) {} //enquanto executa se o retorno for true
+        */
     @endphp  
 
-    @if (count($fornecedores) > 0 && count($fornecedores) < 10)
-        <h3>Existem alguns fornecedores cadastrados</h3>
-    @elseif(count($fornecedores) > 10)
-        <h3>Existem vários fornecedores cadastrados</h3>
-    @else
-        <h3>Ainda não existem fornecedores cadastrados</h3>
+    {{-- @unless executa se o retorno for false --}}
+
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+
+    @if (!($fornecedores[0]['status'] == 'S'))
+            Fornecedor inativo
     @endif
+    <br>
+    @unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condicao for false --> 
+            Fornecedor inativo
+    @endunless
