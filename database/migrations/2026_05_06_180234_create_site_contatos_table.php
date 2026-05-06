a5bfe3bc18d4dda6ead841ccc8f59c54c10f1aa8
igor <igorbernardorocha@gmail.com>
2026-05-06 15:23:34 -0300

    Migration/Sqlite

--- database/migrations/2026_05_06_180234_create_site_contatos_table.php
+++ database/migrations/2026_05_06_180234_create_site_contatos_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name', 50);
+            $table->string('telefone', 20);
+            $table->string('email', 80);
+            $table->interger('motivo_contato');
+            $table->text('mensagem', 50);
         });
     }
 
