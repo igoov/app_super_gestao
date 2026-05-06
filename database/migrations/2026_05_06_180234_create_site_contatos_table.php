a0ab7221f41ec755adbdfd89612d55df8ed4b948
igor <igorbernardorocha@gmail.com>
2026-05-06 15:46:10 -0300

    Mysql

--- database/migrations/2026_05_06_180234_create_site_contatos_table.php
+++ database/migrations/2026_05_06_180234_create_site_contatos_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('name', 50);
             $table->string('telefone', 20);
             $table->string('email', 80);
-            $table->interger('motivo_contato');
-            $table->text('mensagem', 50);
+            $table->integer('motivo_contato');
+            $table->text('mensagem');
         });
     }
 
